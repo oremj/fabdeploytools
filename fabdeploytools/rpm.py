aefed0a85d978e720f3b0afb1885a8ff39452ab7
Jeremiah Orem <oremj@oremj.com>
2013-06-25 11:12:12 -0700

    Send packages to RPM http server.

--- fabdeploytools/rpm.py
+++ fabdeploytools/rpm.py
@@ -2,12 +2,13 @@ import os
 import time
 from tempfile import NamedTemporaryFile
 
-from fabric.api import local, run, put
+from fabric.api import lcd, local, run, put
 
 
 class RPMBuild:
     def __init__(self, name, env, ref, build_id=None,
-                 install_dir=None, cluster=None, domain=None):
+                 install_dir=None, cluster=None, domain=None,
+                 http_root=None):
         """
         name: codename of project e.g. "zamboni"
         env: prod, stage, dev, etc
@@ -16,6 +17,8 @@ class RPMBuild:
         install_dir: where package will be installed
         cluster, domain: if cluster and domain are present install_dir will
                          be constructed
+        http_root: where packages with be hosted. if cluster and domain are
+                   defined location will be http_root/$cluster/$domain/$package
         """
         self.name = name
         self.env = env
@@ -28,6 +31,11 @@ class RPMBuild:
         else:
             raise Exception('Either install_dir or cluster '
                             'and domain must be defined')
+
+        if cluster and domain and http_root:
+            self.http_root = os.path.join(http_root, cluster, domain)
+        else:
+            self.http_root = http_root
 
         self.package_name = 'deploy-%s-%s' % (self.name, self.env)
         full_name = 'deploy-{0.name}-{0.env}-{0.build_id}-{0.ref}'.format(self)
@@ -85,3 +93,26 @@ class RPMBuild:
 
     def clean(self):
         local('rm -f %s' % self.package_filename)
+
+    def update_package_server(self, rpm=None):
+        """rpm: path to latest rpm"""
+
+        if not self.http_root:
+            raise Exception('http_root is not defined.')
+
+        if not rpm:
+            rpm = self.package_filename
+
+        if not os.path.isfile(rpm):
+            raise Exception('rpm does not exist.')
+
+        latest_sym = os.path.join(self.http_root, 'LATEST')
+        prev_sym = os.path.join(self.http_root, 'PREVIOUS')
+        package = os.path.join(self.http_root, os.path.basename(rpm))
+
+        with lcd(self.http_root):
+            local('mkdir -p %s' % self.http_root)
+            local('cp {0} {1}'.format(rpm, package))
+            local('[[ -h {0} ]] && '
+                  'ln -snf $(readlink {0}) {1}'.format(latest_sym, prev_sym))
+            local('ln -snf {0} {1}'.format(package, latest_sym))
