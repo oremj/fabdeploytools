23c8b164e976b6d92127da4d1784976877a14686
Jeremiah Orem <oremj@oremj.com>
2013-07-04 01:06:12 -0700

    Support host ranges e.g., 'web[1:10].example.com'

--- fabdeploytools/envs.py
+++ fabdeploytools/envs.py
@@ -1,9 +1,31 @@
-from fabric import api
 import json
+import re
+
+from fabric import api
+
+
+def expand_hosts(hosts):
+    new_hosts = []
+    for host in hosts:
+        m = re.search('(.*)\[(\d+):(\d+)\](.*)', host)
+        if m:
+            lower = int(m.group(2))
+            upper = int(m.group(3))
+            head, tail = m.group(1, 4)
+            if lower < upper:
+                for j in range(lower, upper + 1):
+                    new_hosts.append("{0}{1}{2}".format(head, j, tail))
+        else:
+            new_hosts.append(host)
+
+    return new_hosts
 
 
 def loadenv(f):
     roles = json.load(open(f))
+    for role, hosts in roles.iteritems():
+        roles[role] = expand_hosts(hosts)
+
     api.env.roledefs = roles
     all_hosts = []
     for v in roles.values():
