3d0ae15a5c5744173046ff9cc2b7f2d04c5005ee
Jeremiah Orem <oremj@oremj.com>
2013-08-26 10:22:26 -0700

    Add support for ec2.

--- fabdeploytools/envs.py
+++ fabdeploytools/envs.py
@@ -5,6 +5,28 @@ import re
 from fabric import api
 
 
+def aws_hosts_iter(filters):
+    from boto.utils import get_instance_identity
+    from boto.ec2 import connect_to_region
+    region = get_instance_identity()['document']['region']
+    c = connect_to_region(region)
+
+    res = c.get_all_instances(filters=filters)
+    for r in res:
+        for instance in r.instances:
+            yield instance
+
+
+def aws_hosts(filters):
+    boto_filters = {}
+    filters = filters.split(',')
+    for filter_ in filters:
+        k, v = filter_.split(':')
+        boto_filters[k] = v
+
+    return (x.private_ip_address for x in aws_hosts_iter(boto_filters))
+
+
 def expand_hosts(hosts):
     new_hosts = []
     for host in hosts:
@@ -31,7 +53,10 @@ def loadenv(f):
         f = os.path.join('/etc/deploytools/envs', f)
     roles = json.load(open(f))
     for role, hosts in roles.iteritems():
-        roles[role] = expand_hosts(hosts)
+        if hosts[:4] == 'aws:':
+            roles[role] = aws_hosts(hosts[4:])
+        else:
+            roles[role] = expand_hosts(hosts)
 
     api.env.roledefs = roles
     all_hosts = []
