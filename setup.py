e4b83b61c4aa6b888e163a344f5db321911c05e9
Jeremy Orem <jeremy.orem@gmail.com>
2013-11-26 10:33:22 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if os.path.isdir('/etc/bash_completion.d'):
 
 setup(
     name='fabdeploytools',
-    version='0.0.1',
+    version='0.0.2',
     description='Deploy tools for fabric',
     author='Jeremy Orem',
     author_email='oremj@mozilla.com',
